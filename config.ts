ea22acde51ad66fc342c1852a2cf8af2833c89c3
MO7G <mr.mohammed1913@gmail.com>
2024-11-26 04:53:14 +0200

    done

--- config.ts
+++ config.ts
@@ -7,20 +7,21 @@ import UserRoles from "supertokens-node/recipe/userroles";
 import EmailVerification from "supertokens-node/recipe/emailverification";
 
 export function getApiDomain() {
-    const apiPort =  8000;
+    const apiPort = 8000;
     const apiUrl = `http://localhost:${apiPort}`;
     return apiUrl;
-}  
+}
 export function getWebsiteDomain() {
-    const websitePort =  3000;
+    const websitePort = 3000;
     const websiteUrl = `http://localhost:${websitePort}`;
     return websiteUrl;
 }
 export const SuperTokensConfig: TypeInput = {
     supertokens: {
         // this is the location of the SuperTokens core.
-        connectionURI: "https://st-dev-33e43990-a1cc-11ef-bf24-a923520d8a44.aws.supertokens.io",
-        apiKey: "yN-XSzPvfJQ4W-j1p6ymgxFwkj"
+        connectionURI:
+            "https://st-dev-33e43990-a1cc-11ef-bf24-a923520d8a44.aws.supertokens.io",
+        apiKey: "yN-XSzPvfJQ4W-j1p6ymgxFwkj",
     },
     appInfo: {
         appName: "SuperTokens Demo App",
@@ -30,8 +31,83 @@ export const SuperTokensConfig: TypeInput = {
     // recipeList contains all the modules that you want to
     // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
     recipeList: [
-        EmailPassword.init(),
+        EmailPassword.init({
+            override: {
+                functions: (originalImplementation) => {
+                    return {
+                        ...originalImplementation,
+
+                        // override the email password sign up function
+                        signUp: async function (input) {
+                            // TODO: some pre sign up logic
+                            console.log(input);
+
+                            let response = await originalImplementation.signUp(input);
+
+                            if (
+                                response.status === "OK" &&
+                                response.user.loginMethods.length === 1 &&
+                                input.session === undefined
+                            ) {
+                                // TODO: some post sign up logic
+                            }
+
+                            return response;
+                        },
+
+                        // override the email password sign in function
+                        signIn: async function (input) {
+                            // TODO: some pre sign in logic
+
+                            let response = await originalImplementation.signIn(input);
+
+                            if (response.status === "OK" && input.session === undefined) {
+                                // TODO: some post sign in logic
+                            }
+
+                            return response;
+                        },
+                    };
+                },
+            },
+        }),
         ThirdParty.init({
+            override: {
+                functions: (originalImplementation) => {
+                    return {
+                        ...originalImplementation,
+
+                        // override the thirdparty sign in / up function
+                        signInUp: async function (input) {
+                            // TODO: Some pre sign in / up logic
+
+                            let response = await originalImplementation.signInUp(input);
+
+                            if (response.status === "OK") {
+                                let accessToken = response.oAuthTokens["access_token"];
+
+                                let firstName =
+                                    response.rawUserInfoFromProvider.fromUserInfoAPI![
+                                    "first_name"
+                                    ];
+
+                                if (input.session === undefined) {
+                                    if (
+                                        response.createdNewRecipeUser &&
+                                        response.user.loginMethods.length === 1
+                                    ) {
+                                        // TODO: some post sign up logic
+                                    } else {
+                                        // TODO: some post sign in logic
+                                    }
+                                }
+                            }
+
+                            return response;
+                        },
+                    };
+                },
+            },
             signInAndUpFeature: {
                 providers: [
                     // We have provided you with development keys which you can use for testing.
@@ -61,27 +137,12 @@ export const SuperTokensConfig: TypeInput = {
                     },
                     {
                         config: {
-                            thirdPartyId: "apple",
-                            clients: [
-                                {
-                                    clientId: "4398792-io.supertokens.example.service",
-                                    additionalConfig: {
-                                        keyId: "7M48Y4RYDL",
-                                        privateKey:
-                                            "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
-                                        teamId: "YWQCXGJRJL",
-                                    },
-                                },
-                            ],
-                        }, 
-                    },
-                    {
-                        config: {
                             thirdPartyId: "twitter",
                             clients: [
                                 {
                                     clientId: "4398792-WXpqVXRiazdRMGNJdEZIa3RVQXc6MTpjaQ",
-                                    clientSecret: "BivMbtwmcygbRLNQ0zk45yxvW246tnYnTFFq-LH39NwZMxFpdC",
+                                    clientSecret:
+                                        "BivMbtwmcygbRLNQ0zk45yxvW246tnYnTFFq-LH39NwZMxFpdC",
                                 },
                             ],
                         },
@@ -90,10 +151,10 @@ export const SuperTokensConfig: TypeInput = {
             },
         }),
         EmailVerification.init({
-            mode: "REQUIRED", // or "OPTIONAL"
-          }), 
+            mode: "OPTIONAL", // or "OPTIONAL"
+        }),
         Session.init(),
         Dashboard.init(),
-        UserRoles.init(), 
+        UserRoles.init(),
     ],
 };
